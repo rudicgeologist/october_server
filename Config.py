f8783da51fd1fd4175f14da474a18e2d6b669b57
rudicgeologist <rudicgeologist@proton.me>
2024-12-23 21:04:18 +0300

    0.1

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ HOST = "localhost"
 # SERVER_HOST = "localhost"
 
 SERVER_PORT = os.environ.get('PORT',  8765)
-SERVER_HOST = "ws://october-server.onrender.com"
+SERVER_HOST = "localhost"
 
 # 3.75.158.163
 # 3.125.183.140
